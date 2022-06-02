7872af4026905bb911d91fe951368201241de2d8
Administrator <Administrator@PC797>
2022-06-02 15:44:18 +0700

    Add project files.

---
+++ TranNgocTan_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TranNgocTan_BigSchool.Models;
+
+namespace TranNgocTan_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Categories.ToList();
+
+
+            return View(objCourse);
+        }
+    }
+}
---
+++ TranNgocTan_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TranNgocTan_BigSchool.Startup))]
+namespace TranNgocTan_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
